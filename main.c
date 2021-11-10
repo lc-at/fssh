6304b0692e169315aa4cafc1cf1dc8eaa33210e5
loncat <me@lcat.dev>
2021-11-11 00:50:40 +0800

    Started working on proxying ssh

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-        printf("number of argv: %d\n", argc);
-        printf("%s\n", argv[0]);
-        return 0;
+	int pd[2];
+	pid_t pid;
+
+	if (pipe(pd) == -1) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	pid = fork();
+	if (pid == -1) {
+		perror("fork");
+		exit(2);
+	} else if (pid == 0) {
+		execv("/usr/bin/ssh", argv);
+	} else {
+		// wait(NULL);
+	}
+	return 0;
 }
