ad233318ae3413412440e79ba75692790453054b
loncat <me@lcat.dev>
2021-11-11 19:52:24 +0800

    Minimally working version

--- main.c
+++ main.c
@@ -1,26 +1,68 @@
-#include <stdio.h>
+#define _XOPEN_SOURCE 600
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <termios.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-	int pd[2];
-	pid_t pid;
+	char *slavename;
+	int masterfd;
+	int slavefd;
+	pid_t child;
+	char buf;
+	FILE *output;
+	static struct termios oldt, newt;
 
-	if (pipe(pd) == -1) {
-		perror("pipe");
+	tcgetattr(STDIN_FILENO, &oldt);
+
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+	masterfd = posix_openpt(O_RDWR);
+	grantpt(masterfd);
+	unlockpt(masterfd);
+	slavename = ptsname(masterfd);
+	slavefd = open(slavename, O_RDWR);
+
+	signal(SIGINT, SIG_IGN);
+
+	child = fork();
+	if (child == -1) {
+		perror("fork failed");
 		exit(EXIT_FAILURE);
-	}
-
-	pid = fork();
-	if (pid == -1) {
-		perror("fork");
-		exit(2);
-	} else if (pid == 0) {
+	} else if (child == 0) {
+		dup2(slavefd, STDIN_FILENO);
 		execv("/usr/bin/ssh", argv);
 	} else {
-		// wait(NULL);
+		output = fopen("output", "a");
+		setbuf(output, NULL);
+		while (waitpid(child, NULL, WNOHANG) == 0) {
+			int b = read(STDIN_FILENO, &buf, 1);
+
+			if (b == -1) {
+				perror("stdin read error");
+				exit(EXIT_FAILURE);
+			} else if (b == 0) {
+				break;
+			} else if (write(masterfd, &buf, 1) == -1) {
+				perror("write to pipe error");
+				exit(EXIT_FAILURE);
+			}
+			fputc(buf, output);
+		}
+
+		fclose(output);
 	}
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	close(masterfd);
+	close(slavefd);
+
 	return 0;
 }
